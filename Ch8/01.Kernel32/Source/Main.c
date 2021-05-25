0ad237330202a8c3befe79337f45c06daefba907
Yeongbae Jeon <ybjeon01@naver.com>
2021-05-25 08:51:26 +0000

    Ch08: add code for initializing IA-32e mode kernel memory area

--- Ch8/01.Kernel32/Source/Main.c
+++ Ch8/01.Kernel32/Source/Main.c
@@ -1,9 +1,15 @@
 #include "Types.h"
 
 void kPrintString(int iX, int iY, const char *pcString);
+BOOL kInitializeKernel64Area(void);
 
 void Main(void) {
 	kPrintString(0, 3, "C Language Kernel Started~!!!");
+
+    // Initialize IA-32e mode kernel area with value of 0x00
+    // area: 1MB ~ 6MB
+    kInitializeKernel64Area();
+    kPrintString(0, 4, "IA-32e Kernel Area Initialization Complete");
 
 	while (1);
 }
@@ -17,4 +23,21 @@ void kPrintString(int iX, int iY, const char *pcString) {
     for (i = 0; pcString[i] != 0; i++) {
     	pstScreen[i].bCharacter = pcString[i];
     }
+}
+
+// initialize IA-32e mode kernel area with 0x00
+// the area for IA-32e mode Kernel is [1MB, 6MB)
+BOOL kInitializeKernel64Area(void) {
+    DWORD* pdwCurrentAddress = (DWORD*) 0x100000;
+
+    while ( (DWORD) pdwCurrentAddress < 0x600000) {
+        *pdwCurrentAddress = 0x00;
+
+        // check if 0x00 is written to current addr
+        if (*pdwCurrentAddress != 0) {
+            return FALSE;
+        }
+        pdwCurrentAddress++;
+    }
+    return TRUE;
 }
