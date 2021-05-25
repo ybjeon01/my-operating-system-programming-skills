1e64069869295f800ce58a2ffab3cb35682c32fb
Yeongbae Jeon <ybjeon01@naver.com>
2021-05-25 10:06:19 +0000

    Ch08: activate A20 Gate so kernel can access above 1MB

--- Ch8/01.Kernel32/Source/EntryPoint.s
+++ Ch8/01.Kernel32/Source/EntryPoint.s
@@ -8,23 +8,40 @@ START:
     mov ds, ax
     mov es, ax
 
+    ;; A20 Gate activation
+    ; activate A20 by using BIOS service, if it fails
+    ; try activating by using system control port
+    mov ax, 0x2401 ; 0x2401: activate
+    int 0x15       ; 0x15: miscellaneous system services
+
+    jc .A20GATEERROR ; if cflag is set, it fails
+    jmp .A20GATESUCCESS
+
+    .A20GATEERROR:
+        in al, 0x92  ; 0x92: system control port
+        or al, 0x02  ; set A20 Gate bit (set bit 1)
+        and al, 0xFE ; prevent reset (clear bit 0)
+        out 0x92, al
+
+    .A20GATESUCCESS:
+
     cli ; ignore interrupt until interrupt handler is set
 
     lgdt [GDTR]
     
-    ; PG=0, CD=1, NW=0, AM=0, WP=0, NE=0, ET=1, EM=0, MP=1, PE=1
-    ; CD means cache disable
-    ; PE is protection enable    
-    ; EM, ET, MP, PE are FPU-related fields and they are set
-    ; although their feature is not used in IA-32 mode. they will
-    ; be explained in detail later. Don't worry about the fields now
-    mov eax, 0x4000003B
-    mov cr0, eax
+        ; PG=0, CD=1, NW=0, AM=0, WP=0, NE=0, ET=1, EM=0, MP=1, PE=1
+        ; CD means cache disable
+        ; PE is protection enable    
+        ; EM, ET, MP, PE are FPU-related fields and they are set
+        ; although their feature is not used in IA-32 mode. they will
+        ; be explained in detail later. Don't worry about the fields now
+        mov eax, 0x4000003B
+        mov cr0, eax
 
-    ; 0x08 is offset to the code segment descriptor from GDT
-    ; PROTECTEDMODE - $$ + 0x10000 is offset to the code from
-    ; segment
-    jmp dword 0x08: (PROTECTEDMODE - $$ + 0x10000)
+        ; 0x08 is offset to the code segment descriptor from GDT
+        ; PROTECTEDMODE - $$ + 0x10000 is offset to the code from
+        ; segment
+        jmp dword 0x08: (PROTECTEDMODE - $$ + 0x10000)
 
 [BITS 32]
 PROTECTEDMODE:
